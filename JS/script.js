b50c47d3e25dfe46288537ad8cc56efb05efe45d
Bruno Carvalho <bruno@MBP-de-Bruno.lan>
2020-08-04 23:10:03 +0100

    CHANGES

--- JS/script.js
+++ JS/script.js
@@ -3,12 +3,25 @@ $(document).ready(function() {
     //Progress Bar
     $(window).scroll(function() {
 
-        var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        var scrolled = (winScroll / height) * 100;
+        let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        let scrolled = (winScroll / height) * 100;
     
         document.querySelector("#bar").style.height = scrolled + "%";
     });
 
     
+    //Titulo Cortado
+    $(window).onresize(function() {
+        let offset = $( ".left" ).offset();
+        let left = offset.left + 10
+        document.querySelector(".left").style.left =  "-" + left + "px";
+    });
+
+
+    //Scroll Down
+    $("#scroll").click(function() {
+        let height = $('.hero .wrapper article').outerHeight() - 100;
+        $('html, body').animate({scrollTop: height}, 800);
+    });
 });
