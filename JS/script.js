6aa982d444179876b677b62827071ffe29eab107
Bruno Carvalho <bruno@MBP-de-Bruno.lan>
2020-08-05 14:17:42 +0100

    CHANGE

--- JS/script.js
+++ JS/script.js
@@ -1,22 +1,21 @@
 $(document).ready(function() {
-
-    //Progress Bar
-    $(window).scroll(function() {
-
-        let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
-        let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
-        let scrolled = (winScroll / height) * 100;
-    
-        document.querySelector("#bar").style.height = scrolled + "%";
-    });
-
     
     //Titulo Cortado
-    $(window).onresize(function() {
-        let offset = $( ".left" ).offset();
-        let left = offset.left + 10
-        document.querySelector(".left").style.left =  "-" + left + "px";
-    });
+    left();
+    window.addEventListener('resize', left);
+
+    function left() {
+        let offset = $( ".hero .wrapper" ).offset();
+
+        if (window.innerWidth >= 1289) {
+            let left = offset.left + 10;
+            document.querySelector(".left").style.left =  "-" + left + "px";
+        }
+        else {
+            let left = offset.left + 70;
+            document.querySelector(".left").style.left =  "-" + left + "px";
+        }
+    }
 
 
     //Scroll Down
@@ -24,4 +23,15 @@ $(document).ready(function() {
         let height = $('.hero .wrapper article').outerHeight() - 100;
         $('html, body').animate({scrollTop: height}, 800);
     });
+
+
+    //Progress Bar
+    // $(window).scroll(function() {
+
+    //     let winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+    //     let height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+    //     let scrolled = (winScroll / height) * 100;
+    
+    //     document.querySelector("#bar").style.height = scrolled + "%";
+    // });
 });
